8a0c9f0ce543153a170592b461b4c4813af567fd
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-04-21 18:47:26 +0900

    Fixed ) 터미널 오류 수정

--- FE/JS/animation.js
+++ FE/JS/animation.js
@@ -4,7 +4,6 @@ const fadeoutText = document.querySelector(".fadeout");
 document.addEventListener("DOMContentLoaded",()=>{
     AOS.init();
     new TypeIt('#typing')
-        .type("저희는 MA:IN# 입니다!")
         .pause(1000)
         .go();
 });
