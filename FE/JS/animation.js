77bcbe8cbe50f6cc57c46ce0d078c105366ab597
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-22 15:25:18 +0900

    animation 로고 , 설명 , 이동 버튼 구현 & 스크롤 이벤트 ) Error scroll

--- FE/JS/animation.js
+++ FE/JS/animation.js
@@ -1,5 +1,17 @@
 const fixedHeader = document.querySelector(".fixedHeader");
 const fadeoutText = document.querySelector(".fadeout");
+const pages = document.querySelector(".pages");
+const home = document.querySelector(".home");
+const homeBtn = document.querySelector("footer").offsetTop;
+const pageBtn = document.querySelector(".animationBtn").offsetTop;
+
+pages.addEventListener("click",()=>{
+    window.scroll({top: document.querySelector(".animationBtn").offsetTop, behavior: 'smooth'});
+});
+
+home.addEventListener("click",()=>{
+    window.scroll({top: document.querySelector(".mainHeader").offsetTop, behavior: 'smooth'});
+});
 
 document.addEventListener("DOMContentLoaded",()=>{
     AOS.init();
@@ -7,3 +19,4 @@ document.addEventListener("DOMContentLoaded",()=>{
         .pause(1000)
         .go();
 });
+
