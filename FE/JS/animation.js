fe6abc56fdaee9e9793387a318042db7371438c0
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-04-22 21:12:09 +0900

    로고 , 홈 버튼 클릭시 스크롤 이동

--- FE/JS/animation.js
+++ FE/JS/animation.js
@@ -1,16 +1,13 @@
-const fixedHeader = document.querySelector(".fixedHeader");
-const fadeoutText = document.querySelector(".fadeout");
-const pages = document.querySelector(".pages");
-const home = document.querySelector(".home");
-const homeBtn = document.querySelector("footer").offsetTop;
-const pageBtn = document.querySelector(".animationBtn").offsetTop;
+document.querySelector(".logo").addEventListener("click",()=>{
+    window.scroll({top: 0 ,behavior: 'smooth'});
+})
 
-pages.addEventListener("click",()=>{
-    window.scroll({top: document.querySelector(".animationBtn").offsetTop, behavior: 'smooth'});
+document.querySelector(".pages").addEventListener("click",()=>{
+    window.scroll({top: 2000 ,behavior: 'smooth'});
 });
 
-home.addEventListener("click",()=>{
-    window.scroll({top: document.querySelector(".mainHeader").offsetTop, behavior: 'smooth'});
+document.querySelector(".home").addEventListener("click",()=>{
+    window.scroll({top: 0 , behavior: 'smooth'});
 });
 
 document.addEventListener("DOMContentLoaded",()=>{
